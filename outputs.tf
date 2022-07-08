b61f9a2b72ebddd481b3fd917fa68f29ce2488dd
Sandro Manke <neuronist@gmail.com>
2022-07-08 22:26:50 +0200

    some docs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,6 @@
 output "kms_key_id" {
-  value = google_kms_crypto_key.this.id
+  value       = google_kms_crypto_key.this.id
+  description = "kms key id"
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,20 +1,24 @@
 variable "kms_key_ring_id" {
   type        = string
-  description = "A key ring is for organizing your keys. In simple projects, you should not need more than one key ring usually. Key rings can not be destroyed, therefore you should be rather careful with this."
+  description = "A key ring is for organizing your keys. In simple projects, you should not need more than one key ring usually. Key rings can not be destroyed, therefore you should be rather careful with this. See also: https://cloud.google.com/kms/docs/resource-hierarchy#key_rings"
 }
 
 variable "name" {
-  type = string
+  type        = string
+  description = "Name of your key"
 }
 
 variable "location" {
-  type = string
+  type        = string
+  description = "Location of the key. Be aware that the resources using the key and the key location need to match. Resources in `global` also need a `global` key and same for `europe` or `europe-west3` etc."
 }
 
 variable "service_identities" {
-  type = list(string)
+  type        = list(string)
+  description = "Service identities for the key."
 }
 
 variable "labels" {
-  type = map(string)
+  type        = map(string)
+  description = "Labels for the key."
 }
